edd82e5cb2a49f8bfa455f4e41abaed14c43e31d
Sebass130201 <sebastianortiz02@hotmail.com>
2021-04-09 13:09:49 -0400

    par

---
+++ asdas.java
@@ -0,0 +1,9 @@
+public class asdas {
+    public static void main(String[] args) { 
+        System.out.println("sOY EL PAOPTE");
+        System.out.pritnyln("im the papote");
+        
+    }
+
+    
+}
